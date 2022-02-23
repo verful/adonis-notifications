ef4be6161592262f0a57349f46d5690222bd26cb
Arthur Rezende <arthur-er@pm.me>
2022-02-23 08:09:50 -0300

    fix: import orm dependencies directly from ioc container

--- src/Models/DatabaseNotification.ts
+++ src/Models/DatabaseNotification.ts
@@ -1,52 +1,52 @@
 import { DateTime } from 'luxon'
 
-import { column, BaseModel } from '@ioc:Adonis/Lucid/Orm'
-import Config from '@ioc:Adonis/Core/Config'
+const { column, BaseModel } = global[Symbol.for('ioc.use')]('Adonis/Lucid/Orm')
+
 import { DatabaseNotificationModel, DatabaseNotificationRow } from '@ioc:Verful/Notification'
 import StaticImplements from '../Helpers/StaticImplements'
 import type { LucidModel } from '@ioc:Adonis/Lucid/Orm'
 
 export default function createNotificationModel(tableName: string): DatabaseNotificationModel {
-@StaticImplements<DatabaseNotificationModel>()
+  @StaticImplements<DatabaseNotificationModel>()
   class DatabaseNotification extends (BaseModel as LucidModel) implements DatabaseNotificationRow {
     public static table = tableName
 
-  @column({ isPrimary: true })
-  public id: number
+    @column({ isPrimary: true })
+    public id: number
 
-  @column({
-    prepare: (value) => JSON.stringify(value),
-    consume: (value) => JSON.parse(value),
-  })
-  public data: Record<string, any>
+    @column({
+      prepare: (value) => JSON.stringify(value),
+      consume: (value) => JSON.parse(value),
+    })
+    public data: Record<string, any>
 
-  @column()
-  public notifiableId: number
+    @column()
+    public notifiableId: number
 
     public async markAsRead(this: DatabaseNotificationRow) {
       await this.merge({ readAt: DateTime.now() }).save()
-  }
+    }
 
     public async markAsUnread(this: DatabaseNotificationRow) {
       await this.merge({ readAt: null }).save()
-  }
+    }
 
-  public get read() {
+    public get read() {
       return Boolean(this.readAt)
-  }
+    }
 
-  public get unread() {
+    public get unread() {
       return !this.readAt
-  }
+    }
 
-  @column.dateTime({ autoCreate: false, autoUpdate: false })
-  public readAt: DateTime | null
+    @column.dateTime({ autoCreate: false, autoUpdate: false })
+    public readAt: DateTime | null
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+    @column.dateTime({ autoCreate: true })
+    public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    public updatedAt: DateTime
   }
 
   return DatabaseNotification
